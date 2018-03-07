6a57496097e3b0a798e8a8d0698f8155e0311151
Vladimir Ivanov <procurator9@gmail.com>
2018-03-07 21:46:07 +0300

    We use PostgreSQL database

--- bet/settings.py
+++ bet/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bet',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
