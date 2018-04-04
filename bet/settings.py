8b08329790faff40fd1a6c440c1c734d2676ab60
Владимир Иванов <ivanov-vi@gollard.ru>
2018-04-04 11:37:05 +0300

    Добавил поиск по матчам

--- bet/settings.py
+++ bet/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 #    'django.contrib.sites',
+    'djorm_pgfulltext',
     'distributedlock',
     'django_bitcoin',
     'tippspiel',
