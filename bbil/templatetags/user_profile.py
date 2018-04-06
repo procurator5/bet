5774d222f2a00bdcb2bfae0c568ea4eb2bdb4ba3
Владимир Иванов <ivanov-vi@gollard.ru>
2018-04-06 17:24:37 +0300

    Показывает состояние счета в $ и евро

--- bbil/templatetags/user_profile.py
+++ bbil/templatetags/user_profile.py
@@ -1,10 +1,14 @@
 from django import template
 from bbil.models import Profile
+from django_bitcoin import  currency
 
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def user_balance(context):
     request = context['request']
-    return str(request.user.profile.wallet.total_balance()) + " " + request.user.profile.currency
+    amount = request.user.profile.wallet.total_balance()
+    if request.user.profile.currency != 'BTC':
+        amount=currency.btc2currency(amount, request.user.profile.currency)    
+    return str(amount) + " " + request.user.profile.currency
     
